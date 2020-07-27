57c713a80339a47dadfbba89b819558772ef888a
lyndexer <mckinney.m93@gmail.com>
2020-07-27 14:04:39 -0700

    AI not saves 4 or less possible next moves on hit, will then run through list, need to narrow again if bot has another succesful hit register from list

--- battle-ship/src/components/Factories/Player.js
+++ battle-ship/src/components/Factories/Player.js
@@ -1,5 +1,6 @@
 function player() {
     let botCoords = []
+    let nextMove = []
     return {
         playerMove(gb, x, y) {
             let board = gb.getBoard()
@@ -13,45 +14,48 @@ function player() {
         botMove(gb) {
             let x = Math.floor(Math.random() * Math.floor(10))
             let y = Math.floor(Math.random() * Math.floor(10))
-            let nextMove = []
             let board = gb.getBoard()
             let markings = gb.getMarkings()
-            if (board[x][y] !== markings.hit && board[x][y] !== markings.miss) {
-                gb.receiveAttack(x, y)
+            if (nextMove.length !== 0) {
+                let randomNextMove = Math.floor(Math.random() * Math.floor(nextMove.length))
+                let a = nextMove[randomNextMove][0]
+                let b = nextMove[randomNextMove][1]
+                gb.receiveAttack(a, b)
                 botCoords = []
-                botCoords.push(x)
-                botCoords.push(y)
+                botCoords.push(a)
+                botCoords.push(b)
+                nextMove.splice(randomNextMove, 1)
+            } else if (nextMove.length === 0) {
+                if (board[x][y] !== markings.hit && board[x][y] !== markings.miss) {
+                    gb.receiveAttack(x, y)
+                    botCoords = []
+                    botCoords.push(x)
+                    botCoords.push(y)
 
-                if (board[x][y] === markings.hit) {
-                    nextMove.push([(x - 1), y])
-                    nextMove.push([(x + 1), y])
-                    nextMove.push([x, (y - 1)])
-                    nextMove.push([x, (y + 1)])
-                    //let randomNextMove = Math.floor(Math.random() * Math.floor(nextMove.length))
-                    for (let i = 0; i < nextMove.length; i++) {
-                        const spot = document.querySelector(`[bot-coords="${nextMove[i][0]},${nextMove[i][1]}"]`);
-                        if (nextMove[i][0] < 0 || nextMove[i][0] > board.length - 1) {
-                            nextMove.splice(i, 1)
-                            i = -1
-                        } else if (nextMove[i][1] < 0 || nextMove[i][1] > board.length - 1) {
-                            nextMove.splice(i, 1)
-                            i = -1
-                            //need to check surrounding area if it has been marked already
-                        } else if (spot.innerHTML === 'X' || spot.innerHTML === 'O') {
-                            nextMove.splice(i, 1)
-                            i = -1
+                    if (board[x][y] === markings.hit) {
+                        nextMove.push([(x - 1), y])
+                        nextMove.push([(x + 1), y])
+                        nextMove.push([x, (y - 1)])
+                        nextMove.push([x, (y + 1)])
+
+                        for (let i = 0; i < nextMove.length; i++) {
+                            const spot = document.querySelector(`[bot-coords="${nextMove[i][0]},${nextMove[i][1]}"]`);
+                            if (nextMove[i][0] < 0 || nextMove[i][0] > board.length - 1) {
+                                nextMove.splice(i, 1)
+                                i = -1
+                            } else if (nextMove[i][1] < 0 || nextMove[i][1] > board.length - 1) {
+                                nextMove.splice(i, 1)
+                                i = -1
+                                //need to check surrounding area if it has been marked already
+                            } else if (spot.innerHTML === 'X' || spot.innerHTML === 'O') {
+                                nextMove.splice(i, 1)
+                                i = -1
+                            }
                         }
-
                     }
-                    console.log(nextMove)
-                    //console.log(randomNextMove)
-
-
-                    //9,4 returns 10, 4
-                    //9.3 returns 10.3
+                } else {
+                    this.botMove(gb)
                 }
-            } else {
-                this.botMove(gb)
             }
         },
         getBotCoords() {
